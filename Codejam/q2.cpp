3b2179768556b83fd2f7dcb343315eabd666daac
RavirajWalke <ravirajwalke9@gmail.com>
2018-04-28 18:37:59 +0530

    CodeJam

---
+++ Codejam/q2.cpp
@@ -0,0 +1,32 @@
+/**
+ *
+ * @author Ravi
+ */
+ #include<stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main()
+{
+    int t,te;
+    cin>>t;
+    for(te=0;te<t;te++)
+    {
+        int N,i,j;
+        cin>>N;
+        ostringstream oss;
+        oss<< N;
+        string Num=oss.str();
+        //cout<<Num;
+        for(i=0;i<Num.length;i++)
+        {
+            if(Num[i]>Num[i+1])
+            {
+
+            }
+        }
+
+    }
+    return 0;
+}
